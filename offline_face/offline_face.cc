eea296b2d31b5426f95d74e26ce3620263a42c40
Henry Cooke <henry@debian.(none)>
2013-02-03 22:44:38 +0000

    added offline_face

---
+++ offline_face/offline_face.cc
@@ -0,0 +1,234 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2010, Jason Mora Saragih, all rights reserved.
+//
+// This file is part of FaceTracker.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+//     * The software is provided under the terms of this licence stricly for
+//       academic, non-commercial, not-for-profit purposes.
+//     * Redistributions of source code must retain the above copyright notice, 
+//       this list of conditions (licence) and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright 
+//       notice, this list of conditions (licence) and the following disclaimer 
+//       in the documentation and/or other materials provided with the 
+//       distribution.
+//     * The name of the author may not be used to endorse or promote products 
+//       derived from this software without specific prior written permission.
+//     * As this software depends on other libraries, the user must adhere to 
+//       and keep in place any licencing terms of those libraries.
+//     * Any publications arising from the use of this software, including but
+//       not limited to academic journal and conference publications, technical
+//       reports and manuals, must cite the following work:
+//
+//       J. M. Saragih, S. Lucey, and J. F. Cohn. Face Alignment through 
+//       Subspace Constrained Mean-Shifts. International Conference of Computer 
+//       Vision (ICCV), September, 2009.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED 
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
+// EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///////////////////////////////////////////////////////////////////////////////
+#include <cstring>
+#include <Tracker.h>
+#include <opencv/highgui.h>
+#include <iostream>
+//=============================================================================
+void Draw(cv::Mat &image,cv::Mat &shape,cv::Mat &con,cv::Mat &tri,cv::Mat &visi)
+{
+  int i,n = shape.rows/2; cv::Point p1,p2; cv::Scalar c;
+
+  //draw triangulation
+  c = CV_RGB(255,255,255);
+  int thickness = 1;
+  for(i = 0; i < tri.rows; i++){
+    if(visi.at<int>(tri.at<int>(i,0),0) == 0 ||
+       visi.at<int>(tri.at<int>(i,1),0) == 0 ||
+       visi.at<int>(tri.at<int>(i,2),0) == 0)continue;
+    p1 = cv::Point(shape.at<double>(tri.at<int>(i,0),0),
+		   shape.at<double>(tri.at<int>(i,0)+n,0));
+    p2 = cv::Point(shape.at<double>(tri.at<int>(i,1),0),
+		   shape.at<double>(tri.at<int>(i,1)+n,0));
+    cv::line(image,p1,p2,c,thickness);
+    p1 = cv::Point(shape.at<double>(tri.at<int>(i,0),0),
+		   shape.at<double>(tri.at<int>(i,0)+n,0));
+    p2 = cv::Point(shape.at<double>(tri.at<int>(i,2),0),
+		   shape.at<double>(tri.at<int>(i,2)+n,0));
+    cv::line(image,p1,p2,c,thickness);
+    p1 = cv::Point(shape.at<double>(tri.at<int>(i,2),0),
+		   shape.at<double>(tri.at<int>(i,2)+n,0));
+    p2 = cv::Point(shape.at<double>(tri.at<int>(i,1),0),
+		   shape.at<double>(tri.at<int>(i,1)+n,0));
+    cv::line(image,p1,p2,c,thickness);
+  }
+  //draw connections
+  c = CV_RGB(0,0,255);
+  for(i = 0; i < con.cols; i++){
+    if(visi.at<int>(con.at<int>(0,i),0) == 0 ||
+       visi.at<int>(con.at<int>(1,i),0) == 0)continue;
+    p1 = cv::Point(shape.at<double>(con.at<int>(0,i),0),
+		   shape.at<double>(con.at<int>(0,i)+n,0));
+    p2 = cv::Point(shape.at<double>(con.at<int>(1,i),0),
+		   shape.at<double>(con.at<int>(1,i)+n,0));
+    cv::line(image,p1,p2,c,1);
+  }
+  //draw points
+  for(i = 0; i < n; i++){    
+    if(visi.at<int>(i,0) == 0)continue;
+    p1 = cv::Point(shape.at<double>(i,0),shape.at<double>(i+n,0));
+    c = CV_RGB(255,0,0); cv::circle(image,p1,2,c);
+  }return;
+}
+//=============================================================================
+int parse_cmd(int argc, const char** argv,
+	      char* ftFile,char* conFile,char* triFile,
+	      bool &fcheck,double &scale,int &fpd,
+        char* inFile,char* outFile,int &blurSize)
+{
+  int i; fcheck = false; scale = 1; fpd = -1;
+  for(i = 1; i < argc; i++){
+    if((std::strcmp(argv[i],"-?") == 0) ||
+       (std::strcmp(argv[i],"--help") == 0)){
+      std::cout << "offline_face:- Hacked together by @prehensile 2013" << std::endl
+     << "based on face_tracker by Jason Saragih 2010" << std::endl
+	   << "Performs offline face tracking" << std::endl << std::endl
+	   << "#" << std::endl 
+	   << "# usage: ./offline_face [options]" << std::endl
+	   << "#" << std::endl << std::endl
+	   << "Arguments:" << std::endl
+	   << "-m <string> -> Tracker model (default: ../model/face2.tracker)"
+	   << std::endl
+	   << "-c <string> -> Connectivity (default: ../model/face.con)"
+	   << std::endl
+	   << "-t <string> -> Triangulation (default: ../model/face.tri)"
+	   << std::endl
+	   << "-s <double> -> Image scaling (default: 1)" << std::endl
+	   << "-d <int>    -> Frames/detections (default: -1)" << std::endl
+     << "-i <string> -> Image file to search" << std::endl
+     << "-o <string> -> Output image filename" << std::endl
+     << "-b <int>    -> Blur size (default 0)" << std::endl
+	   << "--check     -> Check for failure" << std::endl;
+      return -1;
+    }
+  }
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"--check") == 0){fcheck = true; break;}
+  }
+  if(i >= argc)fcheck = false;
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"-s") == 0){
+      if(argc > i+1)scale = std::atof(argv[i+1]); else scale = 1;
+      break;
+    }
+  }
+  if(i >= argc)scale = 1;
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"-d") == 0){
+      if(argc > i+1)fpd = std::atoi(argv[i+1]); else fpd = -1;
+      break;
+    }
+  }
+  if(i >= argc)fpd = -1;
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"-m") == 0){
+      if(argc > i+1)std::strcpy(ftFile,argv[i+1]);
+      else strcpy(ftFile,"../model/face2.tracker");
+      break;
+    }
+  }
+  if(i >= argc)std::strcpy(ftFile,"../model/face2.tracker");
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"-c") == 0){
+      if(argc > i+1)std::strcpy(conFile,argv[i+1]);
+      else strcpy(conFile,"../model/face.con");
+      break;
+    }
+  }
+  if(i >= argc)std::strcpy(conFile,"../model/face.con");
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"-t") == 0){
+      if(argc > i+1)std::strcpy(triFile,argv[i+1]);
+      else strcpy(triFile,"../model/face.tri");
+      break;
+    }
+  }
+  if(i >= argc)std::strcpy(triFile,"../model/face.tri");
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"-i") == 0){
+      if(argc > i+1)std::strcpy(inFile,argv[i+1]);
+      break;
+    }
+  }
+  if(i >= argc) return -1;
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"-o") == 0){
+      if(argc > i+1)std::strcpy(outFile,argv[i+1]);
+      break;
+    }
+  }
+  if(i >= argc) return -1;
+  for(i = 1; i < argc; i++){
+    if(std::strcmp(argv[i],"-b") == 0){
+      if(argc > i+1)blurSize = std::atoi(argv[i+1]); else blurSize = 0;
+      break;
+    }
+  }
+  if(i >= argc) blurSize = 0;
+  return 0;
+}
+//=============================================================================
+int main(int argc, const char** argv)
+{
+  //parse command line arguments
+  char ftFile[256],conFile[256],triFile[256],inFile[256],outFile[256];
+  bool fcheck = false;
+  double scale = 1;
+  int fpd = -1;
+  int blurSize = 0;
+  if(parse_cmd(argc,argv,ftFile,conFile,triFile,fcheck,scale,fpd,inFile,outFile,blurSize)<0)return 0;
+
+  //set other tracking parameters
+  std::vector<int> wSize1(1); wSize1[0] = 7;
+  std::vector<int> wSize2(3); wSize2[0] = 11; wSize2[1] = 9; wSize2[2] = 7;
+  int nIter = 10;
+  double clamp=3, fTol=0.01; 
+  FACETRACKER::Tracker model(ftFile);
+  cv::Mat tri=FACETRACKER::IO::LoadTri(triFile);
+  cv::Mat con=FACETRACKER::IO::LoadCon(conFile);
+  
+  //initialize camera and display window
+  cv::Mat frame,gray,im; std::string text; 
+ 
+  //grab image, resize and flip
+  //IplImage* I = cvQueryFrame(camera);if(!I)return-1; frame = I;
+  cv::Mat I = cv::imread(inFile, 1); frame = I;
+  if(scale == 1)im = frame; 
+  else cv::resize(frame,im,cv::Size(scale*frame.cols,scale*frame.rows));
+  cv::cvtColor(im,gray,CV_BGR2GRAY);
+
+  // blur image
+  if( blurSize > 0 ) cv::medianBlur( gray, gray, blurSize );
+
+  //track this image
+  std::vector<int> wSize; wSize = wSize2; 
+  if(model.Track(gray,wSize,fpd,nIter,clamp,fTol,fcheck) == 0){
+    int idx = model._clm.GetViewIdx();
+    Draw(im,model._shape,con,tri,model._clm._visi[idx]); 
+  } else {
+    std::cout << "Nothing found :(" << std::endl;
+    return -1;
+  }
+  
+  cv::imwrite( outFile, im );
+  
+  return 0;
+}
+//=============================================================================
